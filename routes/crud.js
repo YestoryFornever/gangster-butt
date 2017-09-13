6351ab5708eff53686bfabd01e31ecd7d9b8c9b7
yestoryfornever <guosj90@163.com>
2017-09-13 11:21:24 +0800

    路由参数及返回格式

--- routes/crud.js
+++ routes/crud.js
@@ -8,7 +8,11 @@ router.get('/:obj_type/list',(req,res,next)=>{
 		{},
 		{},
 		(map_list)=>{
-			res.send(map_list);
+			res.send({
+				status:200,
+				msg:'success',
+				data:map_list
+			});
 		}
 	);
 });
@@ -18,36 +22,53 @@ router.post('/:obj_type/create',(req,res,next)=>{
 		{_id:crud.ObjectID(req.params.id)},
 		{},
 		(map_list)=>{
-			res.send(map_list);
+			res.send({
+				status:200,
+				msg:'success',
+				data:map_list
+			});
 		}
 	);
 });
-router.get('/:obj_type/read/:id([0-9]+)',(req,res,next)=>{
+router.get('/:obj_type/read/:id([0-9a-z]+)',(req,res,next)=>{
+	console.log(req.params.id);
 	crud.read(
 		req.params.obj_type,
 		{_id: crud.ObjectID(req.params.id)},
 		{},
 		(map_list)=>{
-			res.send(map_list);
+			res.send({
+				status:200,
+				msg:'success',
+				data:map_list
+			});
 		}
 	);
 });
-router.post('/:obj_type/update/:id([0-9]+)',(req,res,next)=>{
+router.post('/:obj_type/update/:id([0-9a-z]+)',(req,res,next)=>{
 	crud.update(
 		req.params.obj_type,
 		{_id: crud.ObjectID(req.params.id)},
 		req.body,
 		(result_map)=>{
-			res.send(result_map);
+			res.send({
+				status: 200,
+				msg: 'success',
+				data: result_map
+			});
 		}
 	);
 });
-router.get('/:obj_type/delete/:id([0-9]+)',(req,res,next)=>{
+router.get('/:obj_type/delete/:id([0-9a-z]+)',(req,res,next)=>{
 	crud.delete(
 		req.params.obj_type,
 		{_id: crud.ObjectID(req.params.id)},
 		(result_map)=>{
-			res.send(result_map);
+			res.send({
+				status: 200,
+				msg: 'success',
+				data: result_map
+			});
 		}
 	);
 });
